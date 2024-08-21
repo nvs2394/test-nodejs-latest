079631b22e17c9b938357eddd2c5ddb178422ce0
Son Nguyen <nvs2394@gmail.com>
2024-08-21 12:17:17 +0800

    test - mock current date

--- utils/__tests__/date.test.ts
+++ utils/__tests__/date.test.ts
@@ -3,7 +3,8 @@ import test, { describe } from "node:test";
 import { getCurrentDateOfMonth } from "../date";
 
 describe('date', () => {
-  test('should return current date of month', () => {
-    equal(getCurrentDateOfMonth(), new Date().getDate())
+  test('should return current date of month', (context) => {
+    context.mock.timers.enable({ apis: ['Date'], now: new Date(2021, 8, 11) })
+    equal(getCurrentDateOfMonth(), 11)
   })
 })
